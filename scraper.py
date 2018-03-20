cda718fc456e3c3108116287dff351f11428c200
Aaron Rosenberg <arosenbe@rosenberg-co-lm.ngdc.noaa.gov>
2018-03-20 12:01:43 -0600

    scraper documentation

--- scraper.py
+++ scraper.py
@@ -8,13 +8,25 @@ import os
 
 
 def pangea_scrape(target, landing_zone=os.path.join('static', 'data')):
+    """Main function to download files from Pangea generated list to landing
+    zone.
+
+    Args:
+        target (str): Data list path.  Download datalist from Pangea
+            dataset page.
+        landing_zone (str): Download location path.  Defaults to ./static/data.
+
+    Returns:
+        None
+    """
     with open(target) as fid:
         for line in fid:
             items = [x.strip() for x in line.split('\t')]
             if re.match('^\d{4}-.+', items[0]):
                 link = items[-1]
                 download = requests.get(link)
-                with open(os.path.join(landing_zone, os.path.basename(link)), 'w') as fid:
+                download_path = os.path.join(landing_zone, os.path.basename(link))
+                with open(download_path, 'w') as fid:
                     print(link, download.status_code)
                     fid.write(download.content)
 
